ce3859af9a063b70fb9139b172d3ab8db9af622f
Xavier-emmanuel Moreau <xmoreau@z4r5p2.le-101.fr>
2018-09-18 19:00:51 +0200

    ex00 d6 fini

--- Day06/ex00/Color.class.php
+++ Day06/ex00/Color.class.php
@@ -5,51 +5,68 @@ Class Color {
 	public $red = 0;
 	public $green = 0;
 	public $blue = 0;
-	static $verbose = false;
+	public static $verbose = false;
 
-	function __constructor(array $kwargs) {
-
+	function __construct (array $kwargs) {
+		
 		if (array_key_exists('rgb', $kwargs))
 		{
-
+			settype($kwargs['rgb'], "integer");
+			$this->red = ($kwargs['rgb'] & 0xff0000) >> 16;
+			$this->green = ($kwargs['rgb'] & 0x00ff00) >> 8;
+			$this->blue = $kwargs['rgb'] & 0xff;
 		}
-		else if (array_key_exists('red', $kwargs) && array_key_exists('green', $kwargs) & array_key_exists('blue', $kwargs))
+		else if (array_key_exists('red', $kwargs) && array_key_exists('green', $kwargs) && array_key_exists('blue', $kwargs))
 		{
-
+			settype($kwargs['red'], "integer");
+			settype($kwargs['green'], "integer");
+			settype($kwargs['blue'], "integer");
+			$this->red = $kwargs['red'];
+			$this->green = $kwargs['green'];
+			$this->blue = $kwargs['blue'];
 		}
-		else
-			return ;
-		settype($red, "integer");
-		settype($green, "integer");
-		settype($blue, "integer");
-
-		if ($verbose)
+		if (self::$verbose)
+		{	printf('Color( red: %3d, green: %3d, blue :%3d) constructed.', $this->red, $this->green, $this->blue);
+			echo PHP_EOL;}
 		return ;
 	}
 
-	public function doc () {
-		lire depuis Color.doc.txt
+	static function doc () {
+		echo file_get_contents('./Color.doc.txt').PHP_EOL;
 	}
 
 	function __destruct() {
-		if ($verbose)
+		if (self::$verbose)
+		{	printf('Color( red: %3d, green: %3d, blue :%3d) destructed.', $this->red, $this->green, $this->blue);
+			echo PHP_EOL;}
 		return ;
 	}
 
-	function __to_string{
-
-
+	function __tostring () {
+		 return sprintf('Color( red: %3d, green: %3d, blue :%3d)', $this->red, $this->green, $this->blue);
 	}
 
-	function add (){
-
-	}
-
-	function sub (){
-
-	}
-
-	function mult () {
+	function add ($inst){
 		
+		$newinst = new Color( array( 'red' => $this->red + $inst->red, 'green' => $this->green + $inst->green, 'blue' => $this->blue + $inst->blue));
+		$newinst->verbose = self::$verbose;
+		return $newinst;
 	}
-	?>
+
+	function sub ($inst){
+		
+		$newinst = new Color( array( 'red' => $this->red - $inst->red, 'green' => $this->green - $inst->green, 'blue' => $this->blue - $inst->blue));
+		$newinst->verbose = self::$verbose;
+		return $newinst;
+	}
+	
+
+	function mult ($f){
+		
+		$newinst = new Color( array( 'red' => $this->red * $f, 'green' => $this->green * $f, 'blue' => $this->blue * $f));
+		$newinst->verbose = self::$verbose;
+		return $newinst;
+	}
+	
+	}
+?>
--- Day06/ex00/main_00.php
+++ Day06/ex00/main_00.php
@@ -3,8 +3,10 @@
 require_once 'Color.class.php';
 
 
-print( Color::doc() );
+// print( Color::doc() );
 Color::$verbose = True;
+
+
 
 $red     = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0    ) );
 $green   = new Color( array( 'rgb' => 255 << 8 ) );
