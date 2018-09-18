13bf09311926c560f08fa84c440a34e98c3635e8
Xavier-emmanuel Moreau <xmoreau@z4r5p2.le-101.fr>
2018-09-18 11:46:24 +0200

    fin day 5

---
+++ Day05/ex20/ex20.sql
@@ -0,0 +1,4 @@
+SELECT film.id_genre AS id_genre, genre.name AS name_genre, distrib.id_distrib AS id_distrib, distrib.name AS name_distrib, film.title AS title_fim FROM film
+LEFT JOIN genre on film.id_genre = genre.id_genre
+LEFT JOIN distrib on film.id_distrib = distrib.id_distrib
+WHERE film.id_genre BETWEEN 4 and 8;
